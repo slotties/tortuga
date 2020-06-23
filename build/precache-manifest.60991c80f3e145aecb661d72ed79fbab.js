be4c59a39168fe41a7bf9987412b3e6a12dab011
Stefan <slotties@gmail.com>
2020-06-23 09:27:44 +0200

    add gh page

---
+++ build/precache-manifest.60991c80f3e145aecb661d72ed79fbab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce7dd582ba1f5601ccf9b2271670dca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f142741053562fb5018b",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "18775de3c53dd87a995f",
+    "url": "./static/css/main.263d238e.chunk.css"
+  },
+  {
+    "revision": "f142741053562fb5018b",
+    "url": "./static/js/2.5e3e0edc.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "./static/js/2.5e3e0edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18775de3c53dd87a995f",
+    "url": "./static/js/main.dd7f40ba.chunk.js"
+  },
+  {
+    "revision": "181f1c64b4a58d3b7652",
+    "url": "./static/js/runtime-main.d221fa59.js"
+  },
+  {
+    "revision": "7d12919ef9d8cbbeaeb693e81c2df878",
+    "url": "./static/media/Blackpearl-vPxA.7d12919e.woff2"
+  },
+  {
+    "revision": "8184b4db07d138b3b2c50c5065bdc965",
+    "url": "./static/media/Blackpearl-vPxA.8184b4db.ttf"
+  },
+  {
+    "revision": "e0002b97f63ca56634ec247bddd837c9",
+    "url": "./static/media/Blackpearl-vPxA.e0002b97.woff"
+  }
+]);
