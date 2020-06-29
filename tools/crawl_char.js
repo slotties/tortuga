34ca20fea0d38520ba2b9b66aa2ee94d4d74ecb5
Stefan <slotties@gmail.com>
2020-06-29 21:27:59 +0200

    add all the characters

--- tools/crawl_char.js
+++ tools/crawl_char.js
@@ -1,56 +1,50 @@
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
+const charUrls = require('./char_urls.json')
 
 const TARGET_PATH = path.join(__dirname, '../src/data/chars')
-const url = process.argv[2];
 
-if (!url) {
-    console.error('No URL provided.');
-    printUsage();
-    process.exit(1);
-}
+console.log(`Downloading ${charUrls.length} characters to ${TARGET_PATH} ...`);
 
-console.log(url);
+charUrls.forEach(crawl);
 
-https.get(url, (response) => {
-    let body = '';
-    response.on('data', (chunk) => { body += chunk });
-    response.on('end', () => {
-        const id = extractId(body);
-        const name = extractName(body);
-        const costs = extractCosts(body);
-        const type = guessType(body);
+function crawl(url) {
+    console.log(`Downloading ${url} ...`);
+    https.get(url, (response) => {
+        let body = '';
+        response.on('data', (chunk) => { body += chunk });
+        response.on('end', () => {
+            const id = extractId(body);
+            const name = extractName(body);
+            const costs = extractCosts(body);
+            const type = guessType(body);
 
-        if (!id || !name) {
-            console.error('Could not extract ID or name');
-            process.exit(2);
-        }
+            if (!id || !name) {
+                console.error(`Could not extract ID or name (URL: ${url})`);
+                return;
+            }
 
-        const mainFaction = id.substring(0, 3);
-        const dataJson = {
-            "id": id,
-            "name": name,
-            "factions": [ mainFaction ],
-            "costs": costs,
-            "type": type,
-            "url": url
-        };
+            const mainFaction = id.substring(0, 3);
+            const dataJson = {
+                "id": id,
+                "name": name,
+                "factions": [ mainFaction ],
+                "costs": costs,
+                "type": type,
+                "url": url
+            };
 
-        const fileName = path.join(TARGET_PATH, id.toLowerCase() + '.json');
-        console.log(`Saving to ${fileName}.`);
-        fs.writeFile(fileName, JSON.stringify(dataJson, null, 4), () => {
-            process.exit(0);
-        });
-    });    
-});
-
-function printUsage() {
-    console.log(`Usage: node ${process.argv[1]} [SHOP_URL]`)
+            const fileName = path.join(TARGET_PATH, id.toLowerCase() + '.json');
+            fs.writeFile(fileName, JSON.stringify(dataJson, null, 4), () => {
+                console.log(`Saved ${fileName}.`);
+            });
+        });    
+    });
 }
 
 function extractId(html) {
-    const match =  html.match(/<h1>([A-Z]{3}) ([0-9]{3})/);
+    const match =  html.match(/<h1>([A-Z]{3}) ([\w-]+)/);
     if (!match || match.length === 0) {
         return null;
     }
@@ -59,7 +53,7 @@ function extractId(html) {
 }
 
 function extractName(html) {
-    const match =  html.match(/<h1>[A-Z]{3} [0-9]{3} (.*)<\/h1>/);
+    const match =  html.match(/<h1>[A-Z]{3} [\w-]+ (.*)<\/h1>/);
     if (!match || match.length === 0) {
         return null;
     }
